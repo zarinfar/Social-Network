b606034c309e50842b14b58ec1f82aff2be129e2
maminzarinfar1001 <maminzarinfar1001@gmail.com>
2016-03-02 23:20:08 +0330

    in first commit add app/auth file in git

---
+++ app/auth/__init__.py
@@ -0,0 +1,4 @@
+from flask.blueprints import Blueprint
+
+auth = Blueprint('auth', __name__)
+from . import views
---
+++ app/auth/forms.py
@@ -0,0 +1,33 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    username = StringField('User name', validators=[Required(), Length(1, 64), Regexp('^[A-Za-z][A-za-z0-9_.]*$', 0,
+                                                                                      message='User names must have only letters numbers, dots or underscores')])
+    password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='password must mach')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already Registered')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('user name already in use')
+
+
