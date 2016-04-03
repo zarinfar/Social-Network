ceb020eac0d7d6dbd089eb7992c1d9aa7a327aff
zarinfar <maminzarinfar1001@gmail.com>
2016-04-03 16:24:24 +0430

    94.01.15

--- config.py
+++ config.py
@@ -5,14 +5,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'maminzarinfar1001@gmail.com')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '@M!NMINOO1374')
-    MAIL_USERNAME = 'maminzarinfar1001@gmail.com'
-    MAIL_PASSWORD = '@M!NMINOO1374'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
