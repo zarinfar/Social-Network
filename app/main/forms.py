92bcedf61f892d527cab43f7848ba3e87b485035
zarinfar <maminzarinfar1001@gmail.com>
2016-04-10 17:11:27 +0430

    95.01.21 user  Edit profile is ok

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import Required, length
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import Required, Length, Email, Regexp, ValidationError
+from ..models import Role, User
 
 
 class NameForm(Form):
@@ -9,8 +10,33 @@ class NameForm(Form):
 
 
 class EditProfileForm(Form):
-    name = StringField('Real Name', validators=[length(0, 64)])
-    location = StringField('Location', validators=[length(0, 64)])
+    name = StringField('Real Name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About Me')
     submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                                                     'Usernames must have only letters, numbers, dots or underscores')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real Name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(1, 64)])
+    about_me = TextAreaField('About Me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
 
--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
-from flask import current_app
+from flask import current_app, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
+import hashlib
 
 
 class Permission:
@@ -64,6 +65,15 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'https://www.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
+
 
     # def __init__(self, **kwargs):
     #     super(User, self).__init__(**kwargs)
